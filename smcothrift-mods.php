5c03e6fbc8f3b9e930fd492ca64017740f2e107e
Michael Wender <michael@michaelwender.com>
2020-08-25 12:11:47 -0400

    Documenting 1.4.0

--- smcothrift-mods.php
+++ smcothrift-mods.php
@@ -7,7 +7,7 @@
  * Author URI:      https://mwender.com
  * Text Domain:     smcothrift-mods
  * Domain Path:     /languages
- * Version:         1.3.1
+ * Version:         1.4.0
  *
  * @package         SmcoThriftMods
  */
