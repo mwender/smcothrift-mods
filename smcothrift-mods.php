a772d5475f707f2dfce71c9b711023c3fdf1081e
Michael Wender <michael@michaelwender.com>
2020-08-28 07:02:44 -0400

    Adding `!important` to GF field label margin declaration

--- smcothrift-mods.php
+++ smcothrift-mods.php
@@ -7,7 +7,7 @@
  * Author URI:      https://mwender.com
  * Text Domain:     smcothrift-mods
  * Domain Path:     /languages
- * Version:         1.5.0
+ * Version:         1.5.1
  *
  * @package         SmcoThriftMods
  */
