3bcb02f610d85173bc516c38ecf1538a376574e7
fix <>
2021-01-22 11:07:44 -0500

    Renaming files

--- lib/fns/shortcodes.thriftpoints.php
+++ lib/fns/shortcodes.thriftpoints.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace ThriftTrac\shortcodes;
+namespace ThriftPoints\shortcodes;
 
-function thrifttrac_pricing_form( $atts ){
+function pricing_form( $atts ){
   $args = shortcode_atts([
     'gravityform'                    => null,
     'gf_form_price_display_field_id' => null,
     ''
   ], $atts);
 
-  $file = dirname( __FILE__ ) . '/../html/pricing.thrifttrac.html';
+  $file = dirname( __FILE__ ) . '/../html/pricing.thriftpoints.html';
   $html = ( file_exists( $file ) )? file_get_contents( $file ) : '<p class="alert"><strong>ERROR:</strong> I could not find <code>' . basename( $file ) . '</code>.</p>' ;
 
   if( is_numeric( $args['gravityform'] ) ){
@@ -17,13 +17,14 @@ function thrifttrac_pricing_form( $atts ){
     $html = str_replace( '{{form}}', $form, $html );
   }
 
+//  gravity_form_enqueue_scripts( $args['gravityform'], true );
   wp_enqueue_style( 'smcothrift-mods' );
-  wp_enqueue_script( 'thrifttrac-pricing' );
-  wp_localize_script( 'thrifttrac-pricing', 'wpvars', [
+  wp_enqueue_script( 'thriftpoints-pricing' );
+  wp_localize_script( 'thriftpoints-pricing', 'wpvars', [
     'formId'          => $args['gravityform'],
     'pricingFieldId'  => $args['gf_form_price_display_field_id'],
   ]);
 
   return $html;
 }
-add_shortcode( 'thrifttrac_pricing', __NAMESPACE__ . '\\thrifttrac_pricing_form' );
+add_shortcode( 'thriftpoints_pricing', __NAMESPACE__ . '\\pricing_form' );
